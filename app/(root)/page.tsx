054225a059632c0857211c5ad99be2a5ce6e4262
wobbethan <wobbethan@gmail.com>
2024-04-29 01:13:29 -0400

    auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Threads</h1>
+      <UserButton afterSignOutUrl="/"></UserButton>
+    </main>
+  );
+}
