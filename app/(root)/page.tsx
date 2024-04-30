f67e42735f00d8156b58bbf0ce4da6564b8b5b65
wobbethan <wobbethan@gmail.com>
2024-04-29 21:11:55 -0400

    Home UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-      <UserButton afterSignOutUrl="/"></UserButton>
-    </main>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 }
